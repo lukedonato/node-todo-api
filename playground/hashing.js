922a7ecef2c67fcd1eaa571f1199f926df97e827
Luke Donato <luke.donato@tivix.com>
2017-05-08 16:06:50 -0400

    hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,16 +1,29 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-let data = {
-  id: 10
-};
+let password = '123abc';
 
-let token = jwt.sign(data, '123abc');
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  })
+})
 
-let decoded = jwt.verify(token, '123abc');
+bcrypt.compare(password, 'asdfasdf', (err, result) => {
+  console.log(result); //true or false
+});
 
-console.log(token);
-console.log(decoded);
+// let data = {
+//   id: 10
+// };
+//
+// let token = jwt.sign(data, '123abc');
+//
+// let decoded = jwt.verify(token, '123abc');
+//
+// console.log(token);
+// console.log(decoded);
 // let message = 'I am user number 3';
 // let hash = SHA256(message).toString();
 //
