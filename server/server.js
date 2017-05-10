b11cb34685068b74cea4aeefe0437bddca6f2a3b
Luke Donato <luke.donato@tivix.com>
2017-05-10 11:40:18 -0400

    Add POST /users/login route

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {Todo} = require('./models/todo');
 const {User} = require('./models/user');
 const {ObjectID} = require('mongodb');
 const {authenticate} = require('./middleware/authenticate');
+const bcrypt = require('bcryptjs');
 
 let app = express();
 const port = process.env.PORT;
@@ -109,6 +110,19 @@ app.post('/users', (req, res) => {
 
 app.get('/users/me', authenticate, (req, res) => {
   res.send(req.user);
+});
+
+app.post('/users/login', (req, res) => {
+  let body = _.pick(req.body, ['email', 'password']);
+
+  User.findByCredentials(body.email, body.password).then((user) => {
+    return user.generateAuthToken().then((token) => {
+      res.header('x-auth', token).send(user);
+    });
+  }).catch((e) => {
+    console.log(e);
+    res.status(400).send();
+  });
 });
 
 app.listen(port, () => {
